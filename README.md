d9439a0f5da1e9572aa056af0e1a5e3a95e9b4c3
Carlos Lalimarmo <clalimarmo@gmail.com>
2014-12-09 15:11:58 -0500

    Add the module, update README with usage, dependency and notes

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 Store and retrieve json documents to and from Google Cloud Storage.
 
-    var cloudStore = new jsonGCS({
+## Usage
+
+Require the module, and create an instance, passing in an authenticator
+object, and the name of the bucket where your documents should be stored.
+
+The authenticator object should have a `token` method that returns a
+Google OAuth2 token, which grants the necessary permissions to the named
+bucket.
+
+    JsonGCS = require('json-gcs');
+
+    var cloudStore = new require(JsonGCS({
       authenticator: {
         token: function() { return userOAuth2Token; },
       }
@@ -14,3 +25,16 @@ Store and retrieve json documents to and from Google Cloud Storage.
     cloudStore.get(objectName, function(retrievedDocument) {
       //do something with retrieved json document
     });
+
+## Dependencies
+
+The only dependency at the moment is jQuery, whose `ajax` method is used to
+send request to the Google Cloud Storage JSON API.
+
+At some point this dependency might be removed. Until then, the module
+expects to be able to `require('jquery')`.
+
+## Notes
+
+The module is provided as an AMD module, with simplified CommonJS wrapping.
+It's compatible with RequireJS, I haven't tried it in other contexts.
---
+++ dist/json-gcs.js
@@ -0,0 +1,68 @@
+define(function(require) {
+  var $ = require('jquery');
+
+  //constructor dependency injection helper
+  var ensure = function(dependencyNames, dependencies) {
+    var onlyDependencies = {};
+    for (var i = 0; i < dependencyNames.length; i++) {
+      var expectedDependency = dependencyNames[i];
+      var injectedDependency = dependencies[expectedDependency];
+      if (injectedDependency === undefined || injectedDependency === null) {
+        throw new Error('missing dependency:' + expectedDependency);
+      } else {
+        onlyDependencies[expectedDependency] = injectedDependency;
+      }
+    }
+    return onlyDependencies;
+  };
+
+  var JsonGCS = function(deps) {
+    deps = ensure(['authenticator', 'bucketName'], deps);
+    deps.authenticator = ensure(['token'], deps.authenticator);
+
+    //helpers
+    var authHeader = function() {
+      return 'Bearer ' + deps.authenticator.token();
+    };
+
+    var objectCreateURL = function(name) {
+      return 'https://www.googleapis.com/upload/storage/v1/b/' +
+        deps.bucketName + '/o?uploadType=media&name=' + name;
+    };
+
+    var objectGetURL = function(name) {
+      return 'https://storage.googleapis.com/' + deps.bucketName + '/' + name;
+    };
+
+    //instance methods
+    var instance = {};
+
+    instance.put = function(name, doc) {
+      $.ajax(
+        objectCreateUrl(name),
+        {
+          method: 'POST',
+          headers: { 'Authorization': authHeader() },
+          data: JSON.stringify(doc)
+        }
+      );
+    };
+
+    instance.get = function(name, handleJson) {
+      $.ajax(
+        objectGetURL(name),
+        {
+          headers: { 'Authorization': authHeader() },
+          success: function(doc) {
+            var json = JSON.parse(doc);
+            handleJson(json);
+          }
+        }
+      );
+    };
+
+    return instance;
+  };
+
+  return JsonGCS;
+});
